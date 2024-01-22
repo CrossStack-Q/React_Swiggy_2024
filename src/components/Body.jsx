5366e9ce9fc194f176fe1be74d4915fa80f9efbc
CrossStack-Q <crossstack@gmail.com>
2024-01-22 21:13:48 +0530

    useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import { restrautList } from "../config"
 import Card from "./Card"
 import Shimmer from "./Shimmer";
@@ -15,34 +15,40 @@ function filterData1(searchText, restaurants) {
 
 function Body() {
     const [searchText, setSearchText] = useState("");
-    const [AllRestaurants, setAllRestaurants] = useState(restrautList);
-    const [filteredRestaurants, setFilteredRestaurants] = useState(restrautList);
-    
+    const [AllRestaurants, setAllRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
     console.log("Body Render")
 
-    // useEffect(()=>{
-    //     // API Call
-    //     getRestaurants();
-    // },[])
+    useEffect(() => {
+        // API Call
+        getRestaurants();
+    }, [])
 
 
-    async function getRestaurants(){
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4636976&lng=77.3338783&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    async function getRestaurants() {
+        const data = await fetch("https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6295233&lng=77.3123396&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING", {
+            headers: {
+                'x-cors-api-key': 'temp_aed8b58531dda8c3b5158b1e4016365f'
+            }
+        });
         const json = await data.json();
-        setAllRestaurants(json?.data?.cards)
-        setFilteredRestaurants(json?.data?.cards)
-        console.log(json )
+        const actualData= (json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setAllRestaurants(actualData)
+        setFilteredRestaurants(actualData)
+        console.log(actualData);
+        // console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
     // if(!AllRestaurants) return null;
 
 
-    
-
-    
 
 
-    return AllRestaurants.length===0 ? (<Shimmer/>) :(
+
+
+
+    return AllRestaurants.length === 0 ? (<Shimmer />) : (
         <>
 
             <div className="search-container">
@@ -51,25 +57,25 @@ function Body() {
                 }} />
                 <button className="search-btn" onClick={() => {
                     const data = filterData1(searchText.toLowerCase(), AllRestaurants);
-                    
-                    setFilteredRestaurants (data);
-                    
+
+                    setFilteredRestaurants(data);
+
                 }} >
                     Search
                 </button>
             </div>
-            {filteredRestaurants?.length===0 ? <h1>No Item Found</h1> : (
+            {filteredRestaurants?.length === 0 ? <h1>No Item Found</h1> : (
                 <div className="cardContainer">
-                {
-                    filteredRestaurants?.map(restaurant => {
-                        return (
-                            <Card {...restaurant.info} key={restaurant.info.id} />
-                        )
-                    })
-                }
-            </div>
-            ) }
-            
+                    {
+                        filteredRestaurants?.map(restaurant => {
+                            return (
+                                <Card {...restaurant.info} key={restaurant.info.id} />
+                            )
+                        })
+                    }
+                </div>
+            )}
+
         </>
     )
 }
